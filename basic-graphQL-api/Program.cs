c4578abcba933812492c451ac9311d0fd310780a
Nobeen Islam <nobeen.islam@xdesign.com>
2023-07-07 23:54:14 +0100

    Add sqlLite

--- basic-graphQL-api/Program.cs
+++ basic-graphQL-api/Program.cs
@@ -1,8 +1,14 @@
+using basic_graphQL_api.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,6 +26,5 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
 
 app.Run();

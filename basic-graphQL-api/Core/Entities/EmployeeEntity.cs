aeb1ff49c436fde4a8af9a00acc9c663d44d65c6
Nobeen Islam <nobeen.islam@xdesign.com>
2023-07-07 17:05:03 +0100

    Create employee entity

---
+++ basic-graphQL-api/Core/Entities/EmployeeEntity.cs
@@ -0,0 +1,9 @@
+namespace basic_graphQL_api.Core.Entities;
+
+public class EmployeeEntity
+{
+    public int Id;
+    public string FirstName;
+    public string SecondName;
+    public string Position;
+}
